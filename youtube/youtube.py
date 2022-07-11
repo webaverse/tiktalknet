3a9a26051626239ac6d3d7aaf882b52c66568892
shawticus <shawmakesmagic@gmail.com>
2022-07-11 16:38:16 -0700

    Update youtube.py

--- youtube/youtube.py
+++ youtube/youtube.py
@@ -1,13 +1,16 @@
 from youtube_tts_data_generator import YTSpeechDataGenerator
 generator = YTSpeechDataGenerator(dataset_name='juice')
 
-# generator.download(links_txt="links.txt")
-
+# this will run all of the functions
 # generator.prepare_dataset('links.txt')
 
+# OR run these individually
+generator.download(links_txt="links.txt")
 
-# generator.split_audios()
-# generator.concat_audios(max_limit=12)
+
+generator.split_audios()
+# max limit in seconds
+generator.concat_audios(max_limit=12)
 generator.finalize_dataset()
 
 print("total audio length is" + str(generator.get_total_audio_length()))
