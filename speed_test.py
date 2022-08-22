68d3bc0d6d08d6c6eacd0e1d27941d353d0c00f3
alextitonis <alexandrostitonis@gmail.com>
2022-08-22 18:22:46 +0300

    updated the speed test script, with more values and better format

--- speed_test.py
+++ speed_test.py
@@ -1,14 +1,25 @@
 import time
 import requests
 
-hosts = [{ 'host': '<other host>', 'time_ran': 0}, { 'host': 'http://voice.webaverse.com/tts', 'time_ran': 0}]
+hosts = [{ 'host': '<other host>', 'last_time_ran': 0, 'total_time_ran': 0, 'avatarage_time_ran': 0}, { 'host': 'http://voice.webaverse.com/tts', 'last_time_ran': 0, 'total_time_ran': 0, 'avatarage_time_ran': 0}]
+i = 0
+loops = 50
+
+while i < loops:
+    for host in hosts:
+        start = time.time()
+        data = { 'voice': '1kpEjZ3YqMN3chKSXODOqayEm581rxj4r', 's': 'Hi there, how are you? What is your name?' }
+        response = requests.get(host['host'], params=data)
+        end = time.time()
+        time_ran = end-start
+        host['last_time_ran'] = time_ran
+        host['total_time_ran'] += time_ran
+    i += 1
 
 for host in hosts:
-    start = time.time()
-    data = { 'voice': '1kpEjZ3YqMN3chKSXODOqayEm581rxj4r', 's': 'Hi there, how are you? What is your name?' }
-    response = requests.get(host['host'], params=data)
-    end = time.time()
-    time_ran = end-start
-    host['time_ran'] = time_ran
-
-print(hosts)
+    host['avatarage_time_ran'] = host['total_time_ran'] / loops
+    print('Host:', host['host'])
+    print('Last time ran:', host['last_time_ran'])
+    print('Total time ran:', host['total_time_ran'])
+    print('Average time ran:', host['avatarage_time_ran'])
+    print('----------------------------------------------------')
